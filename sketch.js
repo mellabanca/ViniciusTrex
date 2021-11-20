4f9788f2b510b607eb93b0dfd0a819ea126a29d1
mellabanca <91677889+mellabanca@users.noreply.github.com>
2021-11-19 21:21:43 -0300

    19/11

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 //criando nossas variáveis
-var trex, trex_running, trex_collided;
+var trex, trex_running, trex_triste;
 var ground, invisibleGround, groundImage;
 var clouds;
 var imgclouds;
 var score;
 var obs1,obs2,obs3,obs4,obs5,obs6;
 var Tempodejogo;
+var gameoverimg,gameover,restartimg,restart;
 
 var grupodenuvens, grupodeobs;
 var jogando = 1;
@@ -16,9 +17,12 @@ function preload(){
   //carrega a animação com imagens
   trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
   //carrega a imagem para o sprite
-  trex_collided = loadImage("trex_collided.png");
+  gameoverimg = loadImage("gameOver.png");
+  restartimg = loadImage ("restart.png");
+
   imgclouds= loadImage("cloud.png");
   groundImage = loadImage("ground2.png");
+  trex_triste = loadAnimation("trex_collided.png");
   obs1 = loadImage("obstacle1.png");
   obs2 = loadImage("obstacle2.png");
   obs3 = loadImage("obstacle3.png");
@@ -33,13 +37,24 @@ function setup() {
   
   //crie um sprite de trex, adiciona a animação a ele e ajusta o tamanho
   trex = createSprite(50,160,20,50);
+
   trex.addAnimation("running", trex_running);
+trex.addAnimation("tristeza", trex_triste);
+
   trex.scale = 0.5;
   
   //crie sprite ground (solo), adiciona a imagem a ele e reinicia o solo
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
+
+  gameover = createSprite(300,80);
+  gameover.addImage("Fim",gameoverimg);
+  gameover.scale = 0.5;
+
+  restart = createSprite(300,120);
+  restart.addImage("tentenovamente",restartimg);
+  restart.scale = 0.5;
   
   //crie um solo invisível
   invisibleGround = createSprite(200,190,400,10);
@@ -56,7 +71,7 @@ function setup() {
   //console.log(rand)
 
   trex.setCollider("circle",0,0,40);
-  trex.debug = true;
+  //trex.debug = true;
 
 //coloca o valor inicial para o tempo de jogo
 Tempodejogo = 0;
@@ -99,12 +114,16 @@ if (ground.x < 0){
 
   if(grupodeobs.isTouching(trex)){
     estadodojogo = gameover;
+    trex.changeAnimation("tristeza",trex_triste);
+    grupodeobs.setLifetimeEach(-1);
+    grupodenuvens.setLifetimeEach(-1);
   }
 
 }else if (estadodojogo === gameover){
   ground.velocityX = 0;
   grupodeobs.setVelocityXEach(0);
   grupodenuvens.setVelocityXEach(0);
+  trex.velocityY = 0; 
 }
 
   //impedir que o trex caia
